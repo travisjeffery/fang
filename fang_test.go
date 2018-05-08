38fe30c15475d504f17d4ad5d639e6b71e3632de
Travis Jeffery <tj@travisjeffery.com>
2018-05-08 17:48:23 -0400

    better go doc

--- fang_test.go
+++ fang_test.go
@@ -1,14 +1,19 @@
 package fang_test
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/travisjeffery/fang"
 )
 
 func ExampleFang() {
 	cli := &cobra.Command{
 		Use: "example",
-		Run: func(c *cobra.Command, args []string) {},
+		Run: func(c *cobra.Command, args []string) {
+			fmt.Printf("db conn: %s\n", viper.GetString("db-conn"))
+		},
 	}
 	f := cli.Flags()
 	fang.F(f).
